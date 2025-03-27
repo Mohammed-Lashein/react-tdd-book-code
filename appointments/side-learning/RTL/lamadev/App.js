6340035fc6e2b2e31c3c85adb971010d78b1edf3
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-03-27 14:22:33 +0200

    test: add more RTL tests

--- appointments/side-learning/RTL/lamadev/App.js
+++ appointments/side-learning/RTL/lamadev/App.js
@@ -1,6 +1,16 @@
 function App() {
   return (
-    <div>App hello lama</div>
+    <div>
+      <section>App hello lama</section>
+      <section>
+        <ul>
+          <li>apple</li>
+          <li>orange</li>
+          <li>pudding</li>
+        </ul>
+      </section>
+      <h1 data-testid='banner'>Hello</h1>
+    </div>
   )
 }
 export default App
--- appointments/side-learning/RTL/lamadev/App.test.js
+++ appointments/side-learning/RTL/lamadev/App.test.js
@@ -1,8 +1,30 @@
 import {render, screen} from "@testing-library/react"
 import App from './App'
-it('is shown on the screen', () => {
-  render(<App/>)
-  const el = screen.getByText(/lama/i)
-
-  expect(el).toBeInTheDocument()
+describe("App", () => {
+  it('is shown on the screen', () => {
+    render(<App/>)
+    const el = screen.getByText(/lama/i)
+  
+    expect(el).toBeInTheDocument()
+  })
+  it("renders a ul", () => {
+    render(<App />)
+    const el = screen.getByRole('list');
+    expect(el).toBeInTheDocument()
+  })
+  it("renders 3 list items within the ul", () => {
+    render(<App />)
+    const listItems = screen.getAllByRole('listitem')
+    expect(listItems).toHaveLength(3)
+  })
+  it('renders apple as the first li item', () => {
+    render(<App />)
+    const listItems = screen.getAllByRole('listitem')
+    expect(listItems[0].textContent).toBe('apple')
+  })
+  it("renders the h1", () => {
+    render(<App />)
+    const el = screen.getByTestId('banner')
+    expect(el).toBeInTheDocument()
+  })
 })
