3674b06a9f08f600a16b3d1fb571fcc6b1a30424
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-28 14:57:00 +0300

    refactor(CustomerForm): change tabs to spaces for better code readability

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,47 +1,52 @@
 import { useState } from "react"
 
-export function CustomerForm({  
-    original = {
-      first_name: '',
-      last_name: '',
-      phone_number: '',
-    }, 
-    onSubmit  
-  }) {
-  // const customer = {first_name}
-  const [customer, setCustomer] = useState(original)
-  function handleTextInputChange(e, inputName) {
-    // Note that the e obj will be passed to this function
-    // since we are expecting args to be passed to it we no longer have access to e object
-    setCustomer({
-      ...customer,
-      [inputName]: e.target.value,
-    })
-  }
-  // function handleFirstNameChange(e) {
-  //   setCustomer({
-  //     ...customer,
-  //     first_name: e.target.value
-  //   })
-  // }
-  return (
-    <form
-      id='customer'
-      onSubmit={() => {
-        console.log("From CustomerForm component first_name value : ")
-        console.log(customer.first_name)
+export function CustomerForm({
+	original = {
+		first_name: "",
+		last_name: "",
+		phone_number: "",
+	},
+	onSubmit,
+}) {
+	// const customer = {first_name}
+	const [customer, setCustomer] = useState(original)
+	function handleTextInputChange(e, inputName) {
+		// Note that the e obj will be passed to this function
+		// since we are expecting args to be passed to it we no longer have access to e object
+		setCustomer({
+			...customer,
+			[inputName]: e.target.value,
+		})
+	}
+	// function handleFirstNameChange(e) {
+	//   setCustomer({
+	//     ...customer,
+	//     first_name: e.target.value
+	//   })
+	// }
+	return (
+		<form
+			id='customer'
+			onSubmit={() => onSubmit(customer)}
+		>
+			<label htmlFor='first_name'>First Name :</label>
+			<input
+				type='text'
+				name='first_name'
+				value={customer.first_name}
+				id='first_name'
+				onChange={(e) => handleTextInputChange(e, e.target.id)}
+			/>
 
-        onSubmit(customer)
-      }}
-    >
-      <label htmlFor='first_name'>First Name :</label>
-      <input
-        type='text'
-        name='first_name'
-        value={customer.first_name}
-        id='first_name'
-        onChange={(e) => handleTextInputChange(e, e.target.id)}
-      />
+			<label htmlFor='last_name'>Last Name :</label>
+			<input
+				type='text'
+				name='last_name'
+				readOnly
+				value={customer.last_name}
+				id='last_name'
+				onChange={(e) => handleTextInputChange(e, e.target.id)}
+			/>
 
       <label htmlFor='last_name'>Last Name :</label>
       <input
@@ -51,7 +56,7 @@ export function CustomerForm({
         value={customer.last_name}
         id='last_name'
         onChange={(e) => handleTextInputChange(e, e.target.id)}
-      />
-    </form>
-  )
+			/>
+		</form>
+	)
 }
