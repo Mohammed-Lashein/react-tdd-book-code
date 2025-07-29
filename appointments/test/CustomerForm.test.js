c26cf2ca1ddf8e1868df7e136da5165b9b3c2a7d
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-29 10:35:31 +0300

    refactor: change act() calls to render() calls in CustomerForm.test.js for easier usage

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -6,9 +6,9 @@ import { fireEvent } from '@testing-library/react'
 describe('CustomerForm', () => {
   let render, container
 
-  beforeEach(() => {
-    ({ render, container } = createContainer())
-  })
+	beforeEach(() => {
+		;({ render, container } = createContainer())
+	})
 
   const form = (id) => container.querySelector(`form[id=${id}]`)
   const expectToBeInputFieldOfTypeText = (formEl) => {
@@ -27,32 +27,21 @@ describe('CustomerForm', () => {
     return container.querySelector(`label[for=${formEl}]`)
   }
 
-  it('renders a form', async () => {
-    await act(async () => {
-      render(<CustomerForm />)
-    })
-    expect(form('customer')).not.toBeNull()
-  })
+	it("renders a form", async () => {
+		await render(<CustomerForm />)
+		expect(form("customer")).not.toBeNull()
+	})
 
-  // test factories start
-  function itRendersAsATextBox(fieldName) {
-    it('renders as a text box', async () => {
-      await act(async () => {
-        render(<CustomerForm />)
-      })
-      expectToBeInputFieldOfTypeText(field(fieldName))
-    })
-  }
-  function itIncludesExistingValuePassedAsAProp(fieldName, value) {
-    it('includes the existing value', async () => {
-      await act(async () => {
-        /*
-          short form of 
-          render(<CustomerForm [fieldName] = 'value'/>)
-          but the above line would cause an error because wrapping the property in square brackets is not valid js
-          */
-        render(<CustomerForm original={{...{ [fieldName]: value }}} />)
-      })
+	// test factories start
+	function itRendersAsATextBox(fieldName) {
+		it("renders as a text box", async () => {
+			await render(<CustomerForm />)
+			expectToBeInputFieldOfTypeText(field(fieldName))
+		})
+	}
+	function itIncludesExistingValuePassedAsAProp(fieldName, value) {
+		it("includes the existing value", async () => {
+			await render(<CustomerForm original={{ ...{ [fieldName]: value } }} />)
 
       expect(field(fieldName).value).toBe(value)
     })
@@ -69,63 +58,50 @@ describe('CustomerForm', () => {
       // first_name will generate First Name :
     }
 
-    it('renders a label', async () => {
-      await act(async () => {
-        render(<CustomerForm />)
-      })
-      expect(labelFor(inputName)).not.toBeNull()
-      expect(labelFor(inputName).textContent).toMatch(createFormattedLabelTextFromInputName(inputName))
-    })
-  }
-  function itHasAnIdThatMatchesTheLabelForAttribute(fieldName) {
-    it('assigns an id that matches the label id', async () => {
-      await act(async () => {
-        render(<CustomerForm />)
-      })
-      expect(field(fieldName).id).toBe(fieldName)
-    })
-  }
-  function itSavesExistingValueOnFormSubmission(fieldName, valueToSaveOnSubmission) {
-    it('saves existing value when submitted', async () => {
-      expect.hasAssertions()
-      await act(async () => {
-        render(
-          <CustomerForm
-            original={{[fieldName]: valueToSaveOnSubmission}}
-            onSubmit={(customer) => expect(customer[fieldName]).toBe(valueToSaveOnSubmission)}
-          />
-        )
-      })
+		it("renders a label", async () => {
+			await render(<CustomerForm />)
 
-      await act(async () => {
-        form('customer').dispatchEvent(new Event('submit', { bubbles: true }))
-      })
-    })
-  }
-  function itSubmitsNewValue(fieldName, newValue) {
-    it('saves new value to state when submitted', async () => {
-      expect.hasAssertions()
-      await act(async () => {
-        render(
-          <CustomerForm
-            original={{...{ [fieldName]: 'existingValue' }}}
-            onSubmit={(customer) => {
-              expect(customer[fieldName]).toBe(newValue)
-            }}
-          />
-        )
-      })
+			expect(labelFor(inputName)).not.toBeNull()
+			expect(labelFor(inputName).textContent).toMatch(createFormattedLabelTextFromInputName(inputName))
+		})
+	}
+	function itHasAnIdThatMatchesTheLabelForAttribute(fieldName) {
+		it("assigns an id that matches the label id", async () => {
+			await render(<CustomerForm />)
 
-      fireEvent.change(field(fieldName), { target: { value: newValue } })
+			expect(field(fieldName).id).toBe(fieldName)
+		})
+	}
+	function itSavesExistingValueOnFormSubmission(fieldName, valueToSaveOnSubmission) {
+		it("saves existing value when submitted", async () => {
+			expect.hasAssertions()
 
-      await act(async () => {
-        // logs a warning
-        // form('customer').submit()
-        // more verbose but works
-        form('customer').dispatchEvent(new Event('submit', { bubbles: true }))
-      })
-    })
-  }
+			await render(
+				<CustomerForm
+					original={{ [fieldName]: valueToSaveOnSubmission }}
+					onSubmit={(customer) => expect(customer[fieldName]).toBe(valueToSaveOnSubmission)}
+				/>
+			)
+
+			await render(form("customer").dispatchEvent(new Event("submit", { bubbles: true })))
+		})
+	}
+	function itSubmitsNewValue(fieldName, newValue) {
+		it("saves new value to state when submitted", async () => {
+			expect.hasAssertions()
+			await render(
+				<CustomerForm
+					original={{ ...{ [fieldName]: "existingValue" } }}
+					onSubmit={(customer) => {
+						expect(customer[fieldName]).toBe(newValue)
+					}}
+				/>
+			)
+			fireEvent.change(field(fieldName), { target: { value: newValue } })
+
+			await render(form("customer").dispatchEvent(new Event("submit", { bubbles: true })))
+		})
+	}
 
   // test factories end
   describe('first name field', () => {
