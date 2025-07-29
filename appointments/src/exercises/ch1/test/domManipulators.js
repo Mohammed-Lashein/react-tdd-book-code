0c012172c923a686897e16502856daad13d8094c
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-29 10:17:16 +0300

    refactor: refactor createContainer().render() to wrap the rendered component in act() instead of calling it manually in every test

--- appointments/src/exercises/ch1/test/domManipulators.js
+++ appointments/src/exercises/ch1/test/domManipulators.js
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
+import { act, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 export const createContainer = () => {
   const container = document.createElement('div')
   return {
-    render: (component) => {
+    render: async (component) => {
       const root = createRoot(container)
-      root.render(
-        <StrictMode>
-          {component}
-        </StrictMode>
-      )
+      await act(async () => {
+        root.render(
+          <StrictMode>
+            {component}
+          </StrictMode>
+        )
+      })
     },
     container, 
   }
