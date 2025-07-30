3f350926482f6fb2f62ecf696f4e7eeed7f33e8e
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-07-30 18:24:57 +0300

    feat: use tailwind in CustomerForm component and try it to inspect the visual appearance

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -28,35 +28,48 @@ export function CustomerForm({
     <form
       id='customer'
       onSubmit={() => onSubmit(customer)}
+      className="flex flex-col pt-2 mx-auto"
     >
-      <label htmlFor='first_name'>First Name :</label>
+      <div className="flex items-center justify-center gap-1 p-1 form-group">
+      <label htmlFor='first_name' className='w-36'>First Name:</label>
       <input
         type='text'
         name='first_name'
         value={customer.first_name}
+        className='border rounded border-slate-500'
         id='first_name'
         onChange={(e) => handleTextInputChange(e, e.target.id)}
       />
+      </div>
 
-      <label htmlFor='last_name'>Last Name :</label>
-      <input
-        type='text'
-        name='last_name'
-        value={customer.last_name}
-        id='last_name'
-        onChange={(e) => handleTextInputChange(e, e.target.id)}
-      />
+      <div className="flex items-center justify-center gap-1 p-1 form-group">
+        <label htmlFor='last_name' className='w-36'>Last Name:</label>
+        <input
+          type='text'
+          name='last_name'
+          value={customer.last_name}
+          className='border rounded border-slate-500'
+          id='last_name'
+          onChange={(e) => handleTextInputChange(e, e.target.id)}
+        />
+      </div>
 
-      <label htmlFor='phone_number'>Phone Number</label>
-      <input
-        type='text'
-        name='phone_number'
-        value={customer.phone_number}
-        id='phone_number'
-        onChange={(e) => handleTextInputChange(e, e.target.id)}
-      />
-
-      <input type="submit" value="Add" />
+      <div className="flex items-center justify-center gap-1 p-1 form-group">
+        <label htmlFor='phone_number' className='w-36'>Phone Number:</label>
+        <input
+          type='text'
+          name='phone_number'
+          value={customer.phone_number}
+          className='border rounded border-slate-500'
+          id='phone_number'
+          onChange={(e) => handleTextInputChange(e, e.target.id)}
+        />
+      </div>
+          
+      <div className="flex items-end justify-center gap-1 p-1 form-group">
+        <span className='inline-block w-36'></span>
+      <input type="submit" value="Add" className='text-white bg-blue-400 rounded cursor-pointer w-44'/>
+    </div>
     </form>
   )
 }
